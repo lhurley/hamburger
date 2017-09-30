8c189aeaaee58857a596873def3d0242d450ee19
Lizzie Hurley <l.hurley58@gmail.com>
2017-09-30 00:34:36 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McWhopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushy Room Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meatless Murger', false);
